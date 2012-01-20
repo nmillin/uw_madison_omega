f786b8ddd005fd15643856d911817c1d57336141
Nate Millin <njmillin@gmail.com>
2012-01-19 22:43:18 -0600

    added secondary logo w/ link back to wisc.edu.  Second image upload???

---
+++ templates/region--user_first.tpl.php
@@ -0,0 +1,14 @@
+<?php 
+/**
+ * @file   path_to_theme() .
+ * Alpha's theme implementation to display a region.
+ */
+//$imagepath2 = base_path() . 'sites/all/themes/uw_madison_omega' . '/images/wordmark.gif';//this was going to omega and not uw_madison_omega.  not sure why.
+$imagepath = drupal_get_path('theme', 'uw_madison_omega') . '/images/wordmark.gif';
+?>
+<div<?php print $attributes; ?>>
+  <div<?php print $content_attributes; ?>>
+    <a href="http://www.wisc.edu/" alt="link to Wisc.edu"><img src="<?php echo $imagepath; ?>" alt="University of WisconsinÐMadison" /></a>
+    <?php print $content; ?>
+  </div>
+</div>
